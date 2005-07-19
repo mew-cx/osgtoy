b39d52c9d02067899b2cc46cfed987cc129072f4
http://mew.cx/ <mew-cx@users.noreply.github.com>
2005-07-19 20:31:02 +0000

    moved from OpenSceneGraph-Data

---
+++ osgtoy/trunk/osgtoy-data/ss3d_0000.lua
@@ -0,0 +1,8 @@
+-- see http://astronomy.swin.edu.au/~pbourke/surfaces/supershape3d/
+
+return osgToy.SuperShape3D {
+name="c_0000",
+resolution = 128,
+ss1_m=2, ss1_n1=0.7, ss1_n2=0.3, ss1_n3=0.2, ss1_a=1, ss1_b=1,
+ss2_m=3, ss2_n1=100, ss2_n2=100, ss2_n3=100, ss2_a=1, ss2_b=1,
+}
