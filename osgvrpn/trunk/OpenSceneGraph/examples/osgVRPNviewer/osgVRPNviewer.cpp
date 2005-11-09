6672121a75efa6d28808e5afea7bb77ef36139b3
http://mew.cx/ <mew-cx@users.noreply.github.com>
2005-11-09 07:53:59 +0000

    much reorg: better conditional compilation of tracker configuration; larger, more navigable scene.

--- osgvrpn/trunk/OpenSceneGraph/examples/osgVRPNviewer/osgVRPNviewer.cpp
+++ osgvrpn/trunk/OpenSceneGraph/examples/osgVRPNviewer/osgVRPNviewer.cpp
@@ -1,4 +1,4 @@
-/* -*-c++-*- OpenSceneGraph - Copyright (C) 1998-2004 Robert Osfield 
+/* -*-c++-*- OpenSceneGraph - Copyright (C) 1998-2005 Robert Osfield 
  *
  * This application is open source and may be redistributed and/or modified   
  * freely and without restriction, both in commericial and non commericial applications,
@@ -9,107 +9,104 @@
  * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
 */
 
-/* file:	examples/osgVRPNviewer/osgVRPNviewer.cpp
- * author:	Mike Weiblen mew@mew.cx 2004-08-14
- * copyright:	(C) 2003-2004 Michael Weiblen
- * license:	OpenSceneGraph Public License (OSGPL)
+/* file:        examples/osgVRPNviewer/osgVRPNviewer.cpp
+ * author:      Mike Weiblen mew@mew.cx
+ * copyright:   (C) 2003-2005 Michael Weiblen
+ * license:     OpenSceneGraph Public License (OSGPL)
+ * $Id 2005-11-01 $
  *
- * references:	http://www.openscenegraph.org/
- *		http://www.vrpn.org/
- *		http://www.mew.cx/osg/
- *
- * depends:	osgVRPN 0.2.1, OSG 0.9.7, VRPN 06.04
+ * references:  http://www.openscenegraph.org/
+ *              http://www.vrpn.org/
+ *              http://www.mew.cx/
 */
 
 #include <osg/ShapeDrawable>
 #include <osg/Geode>
 #include <osg/Group>
-#include <osg/ref_ptr>
+#include <osg/Texture2D>
 #include <osgDB/ReadFile>
 #include <osgProducer/Viewer>
 
-#define USE_TRACKER
-//#define TRACKER_ON_NODE
-
-#ifdef USE_TRACKER //[
 #include "osgVRPN/Tracker.h"
 #include "osgVRPN/TrackerTransform.h"
 #include "osgVRPN/TrackerManipulator.h"
-#endif //]
+
+#define CAMERA_TRACKER_NAME "Tracker0@localhost"
+//#define NODE_TRACKER_NAME "Tracker0@localhost"
 
 ///////////////////////////////////////////////////////////////////////////
 
+osg::Node* buildScene( osgProducer::Viewer* viewer )
+{
+    osg::Group* scene = new osg::Group;
+
+    osg::Geode* geode = new osg::Geode;
+    geode->addDrawable(new osg::ShapeDrawable(new osg::Cone(osg::Vec3(0,0,0),1,2)));
+#if defined(NODE_TRACKER_NAME)
+    osgVRPN::TrackerTransform* xform = new osgVRPN::TrackerTransform;
+    xform->setTracker( new osgVRPN::Tracker( NODE_TRACKER_NAME ) );
+    xform->addChild( geode );
+    scene->addChild( xform );
+#else
+    scene->addChild( geode );
+#endif
+
+    geode = new osg::Geode;
+    geode->addDrawable(new osg::ShapeDrawable(new osg::Box(osg::Vec3(0,0,-2),100,100,0.1)));
+    osg::Texture2D* tex0 = new osg::Texture2D;
+    tex0->setImage( osgDB::readImageFile( "Images/lz.rgb" ) );
+    geode->getOrCreateStateSet()->setTextureAttributeAndModes( 0, tex0, osg::StateAttribute::ON );
+    scene->addChild( geode );
+
+    geode = new osg::Geode;
+    geode->addDrawable(new osg::ShapeDrawable(new osg::Box(osg::Vec3(0,0,0),3,3,0.1)));
+    geode->addDrawable(new osg::ShapeDrawable(new osg::Sphere(osg::Vec3(40,0,0),5)));
+    geode->addDrawable(new osg::ShapeDrawable(new osg::Cone(osg::Vec3(0,40,0),2,9)));
+    geode->addDrawable(new osg::ShapeDrawable(new osg::Cylinder(osg::Vec3(-40,0,0),3,7)));
+    geode->addDrawable(new osg::ShapeDrawable(new osg::Box(osg::Vec3(0,-40,0),7,7,7)));
+    scene->addChild( geode );
+
+    return scene;
+}
+
 int main( int argc, char **argv )
 {
-    // use an ArgumentParser object to manage the program arguments.
     osg::ArgumentParser arguments(&argc,argv);
-
-    // set up the usage document, in case we need to print out how to use this program.
     arguments.getApplicationUsage()->setApplicationName(arguments.getApplicationName());
     arguments.getApplicationUsage()->setDescription(arguments.getApplicationName()+" demonstrates osgVRPN");
     arguments.getApplicationUsage()->setCommandLineUsage(arguments.getApplicationName()+" [options]");
     arguments.getApplicationUsage()->addCommandLineOption("-h or --help","Display this information");
 
-    // construct the viewer.
     osgProducer::Viewer viewer(arguments);
-    viewer.setClearColor( osg::Vec4( 0.5, 0.2, 0.2, 1.0 ) );
+    viewer.setUpViewer( osgProducer::Viewer::STANDARD_SETTINGS );
 
-    // set up the value with sensible default event handlers.
-    viewer.setUpViewer(osgProducer::Viewer::STANDARD_SETTINGS);
-
-#ifdef USE_TRACKER //[
-    osgVRPN::TrackerManipulator* manip = new osgVRPN::TrackerManipulator();
-    unsigned int pos = viewer.addCameraManipulator( manip );
-    viewer.selectCameraManipulator( pos );
-#endif //]
-
-    // get details on keyboard and mouse bindings used by the viewer.
-    viewer.getUsage(*arguments.getApplicationUsage());
-
-    // if user request help write it out to cout.
+    viewer.getUsage( *arguments.getApplicationUsage() );
     if (arguments.read("-h") || arguments.read("--help"))
     {
         arguments.getApplicationUsage()->write(std::cout);
         return 1;
     }
 
-#ifdef USE_TRACKER //[
-    osgVRPN::TrackerTransform* xform = new osgVRPN::TrackerTransform;
-#endif //]
+#if defined(CAMERA_TRACKER_NAME)
+    osgVRPN::TrackerManipulator* manip = new osgVRPN::TrackerManipulator();
+    unsigned int pos = viewer.addCameraManipulator( manip );
+    viewer.selectCameraManipulator( pos );
 
-    osg::ref_ptr<osg::Group> group = new osg::Group;
-    osg::Geode* geode = new osg::Geode;
-    geode->addDrawable(new osg::ShapeDrawable(new osg::Cone( osg::Vec3(0,0,0), 0.5f, 1.0f )));
-    xform->addChild( geode );
-    group->addChild( xform );
-
-    geode = new osg::Geode;
-    geode->addDrawable(new osg::ShapeDrawable(new osg::Box( osg::Vec3(0,0,0), 3.0f, 3.0f, 0.05f )));
-    group->addChild( geode );
-
-    viewer.setSceneData( group.get() );
-    viewer.realize();
-
-#ifdef USE_TRACKER //[
-    osg::ref_ptr<osgVRPN::Tracker> gTracker = new osgVRPN::Tracker( "Tracker0@localhost" );
-#ifdef TRACKER_ON_NODE
-    // attach tracker to cone's transform.
-    xform->setTracker( gTracker );
-#else
-    // attach tracker to camera's manipulator.
-    manip->setTracker( gTracker );
+    manip->setTracker( new osgVRPN::Tracker( CAMERA_TRACKER_NAME ) );
+    manip->setAutoComputeHomePosition(false);
+    manip->setHomeMatrix( osg::Matrix::translate(0,0,10) * osg::Matrixd::rotate(1.2,1,0,0) );
 #endif
-#endif //]
 
+    viewer.setSceneData( buildScene( &viewer ) );
+    viewer.realize();
     while( !viewer.done() )
     {
         viewer.sync();
         viewer.update();
         viewer.frame();
     }
-
     viewer.sync();
     return 0;
 }
 
-/*EOF*/
+// vim: set sw=4 ts=8 et ic ai:
