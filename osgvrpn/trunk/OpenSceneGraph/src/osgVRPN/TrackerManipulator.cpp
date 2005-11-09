42dd4e5a3bd9a0f65432e54e1d4f7b3ef94d9f0b
mew <mew@5d68af97-c12e-0410-8cc7-f28dd97b931a>
2005-11-09 08:05:53 +0000

    deprecated use of setByMatrix(), using setHomeMatrix() instead. added implemetation of home() and computeHomeMatrix(). minor reformatting.

--- osgvrpn/trunk/OpenSceneGraph/src/osgVRPN/TrackerManipulator.cpp
+++ osgvrpn/trunk/OpenSceneGraph/src/osgVRPN/TrackerManipulator.cpp
@@ -1,7 +1,8 @@
-/* file:	src/osgVRPN/TrackerManipulator.cpp
- * author:	Mike Weiblen mew@mew.cx 2003-11-30
- * copyright:	(C) 2003 Michael Weiblen
- * license:	OpenSceneGraph Public License (OSGPL)
+/* file:        src/osgVRPN/TrackerManipulator.cpp
+ * author:      Mike Weiblen mew@mew.cx
+ * copyright:   (C) 2003-2005 Michael Weiblen
+ * license:     OpenSceneGraph Public License (OSGPL)
+ * $Id 2005-11-01 $
 */
 
 // #include <cassert>
@@ -24,26 +25,14 @@ TrackerManipulator::TrackerManipulator( const char* trackerName )
 
 ///////////////////////////////////////////////////////////////////////////
 
-void TrackerManipulator::setByMatrix( const osg::Matrixd& matrix )
-{
-    // TODO how does OSG's use fit with this implementation?
-    _origin = matrix;
-}
-
-void TrackerManipulator::setByInverseMatrix( const osg::Matrixd& matrix )
-{
-    // TODO how does OSG's use fit with this implementation?
-    setByMatrix( osg::Matrixd::inverse(matrix) );
-}
-
 osg::Matrixd TrackerManipulator::getMatrix() const
 {
     osg::Matrixd matrix;
 
     if( _tracker.valid() )
-	matrix = _tracker->getMatrix() * _origin;
+        matrix = _tracker->getMatrix() * _homeMatrix;
     else
-	matrix = _origin;
+        matrix = _homeMatrix;
 
     return matrix;
 }
@@ -53,9 +42,9 @@ osg::Matrixd TrackerManipulator::getInverseMatrix() const
     osg::Matrixd matrix;
 
     if( _tracker.valid() )
-	matrix = osg::Matrixd::inverse(_origin) * _tracker->getInverseMatrix();
+        matrix = osg::Matrixd::inverse(_homeMatrix) * _tracker->getInverseMatrix();
     else
-	matrix = osg::Matrixd::inverse(_origin);
+        matrix = osg::Matrixd::inverse(_homeMatrix);
 
     return matrix;
 }
@@ -67,11 +56,57 @@ void TrackerManipulator::setNode(osg::Node* node)
     _node = node;
     if( _node.get() )
     {
-        const osg::BoundingSphere& boundingSphere=_node->getBound();
-
-	setByMatrix( osg::Matrix::translate( 0, 0, 2*boundingSphere._radius ) *
-		osg::Matrixd::rotate( 0.9, 1, 0, 0 ) );
+        //const osg::BoundingSphere& boundingSphere=_node->getBound();
+        //_modelScale = boundingSphere._radius;
     }
+    if( getAutoComputeHomePosition() ) computeHomeMatrix();
+}
+
+///////////////////////////////////////////////////////////////////////////
+
+void TrackerManipulator::computeHomeMatrix()
+{
+    if( _node.get() )
+    {
+        const osg::BoundingSphere& boundingSphere = _node->getBound();
+        setHomeMatrix( osg::Matrix::translate( 0, 0, 2*boundingSphere._radius ) *
+                osg::Matrixd::rotate( 0.9, 1, 0, 0 ) );
+
+//osg::Matrix viewMatrix;
+//viewMatrix.makeLookAt( bs.center()-osg::Vec3(0,2*bs.radius(),0), bs.center(), osg::Vec3(0,0,1) );
+//setHomeMatrix( viewMatrix );
+
+    }
+}
+
+void TrackerManipulator::setHomePosition(const osg::Vec3d& eye, const osg::Vec3d& center, const osg::Vec3d& up, bool autoComputeHomePosition)
+{
+    setAutoComputeHomePosition( autoComputeHomePosition );
+// FUTURE : integrate with _homeMatrix
+    _homeEye = eye;
+    _homeCenter = center;
+    _homeUp = up;
+}
+        
+void TrackerManipulator::getHomePosition(osg::Vec3d& eye, osg::Vec3d& center, osg::Vec3d& up) const
+{
+// FUTURE : integrate with _homeMatrix
+    eye = _homeEye;
+    center = _homeCenter;
+    up = _homeUp;
+}
+
+void TrackerManipulator::computeHomePosition()
+{
+// FUTURE : integrate with _homeMatrix
+}
+
+void TrackerManipulator::home( const osgGA::GUIEventAdapter& ea, osgGA::GUIActionAdapter& us )
+{
+}
+
+void TrackerManipulator::home( double currentTime )
+{
 }
 
 ///////////////////////////////////////////////////////////////////////////
@@ -81,22 +116,21 @@ void TrackerManipulator::setNode(osg::Node* node)
 // process VRPN messages.
 
 bool TrackerManipulator::handle(
-	const osgGA::GUIEventAdapter& ea, osgGA::GUIActionAdapter& )
+        const osgGA::GUIEventAdapter& ea, osgGA::GUIActionAdapter& )
 {
     switch( ea.getEventType() )
     {
-	case osgGA::GUIEventAdapter::FRAME:
-	{
-	    if( _tracker.valid() )
-		_tracker->update();
-	    return true;
-	}
+        case osgGA::GUIEventAdapter::FRAME:
+        {
+            if( _tracker.valid() ) _tracker->update();
+            return true;
+        }
 
-	default:
-	    break;
+        default:
+            break;
     }
 
     return false;
 }
 
-/*EOF*/
+// vim: set sw=4 ts=8 et ic ai:
