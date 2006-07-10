419ec77f76e5cb37a3c0ad73f3eda17306b4a483
http://mew.cx/ <mew-cx@users.noreply.github.com>
2006-07-10 06:19:54 +0000

    AnalogTracker is now working.

--- osgvrpn/trunk/OpenSceneGraph/include/osgVRPN/AnalogTracker.h
+++ osgvrpn/trunk/OpenSceneGraph/include/osgVRPN/AnalogTracker.h
@@ -1,23 +1,24 @@
 /* file:        include/osgVRPN/AnalogTracker.h
- * author:      Mike Weiblen mew@mew.cx
- * copyright:   (C) 2006 Michael Weiblen
+ * author:      Mike Weiblen
+ * copyright:   (C) 2006 Michael Weiblen http://mew.cx/
  * license:     OpenSceneGraph Public License (OSGPL)
- * $Id: AnalogTracker.h,v 1.2 2006/07/01 20:48:51 mew Exp $
- *
- * references:  http://www.openscenegraph.org/
- *              http://www.vrpn.org/
- *              http://mew.cx/
+ * $Id 2006-07-08 $
 */
 
 #ifndef OSGVRPN_ANALOGTRACKER
 #define OSGVRPN_ANALOGTRACKER 1
 
-#include <osgVRPN/Export>
+#include <osg/Vec3>
+#include <osg/Matrixd>
 #include <osgVRPN/TrackerBase.h>
+#include <osgVRPN/Analog.h>
+#include <osgVRPN/Button.h>
+
+#include <osgVRPN/Export>
 
 namespace osgVRPN {
 
-/** an aggregate of vrpn_Analog_Remote channels. */
+/** a TrackerBase which integrates the output of several vrpn_Analog_Remote channels. */
 
 class OSGVRPN_EXPORT AnalogTracker : public TrackerBase
 {
@@ -25,18 +26,46 @@ public:
     AnalogTracker();
 
     /** Update our state from the device. */
-    bool update();
+    void update();
+
+    void reset() {_matrix.makeIdentity();}
 
     /** Query the tracker's transform matrix */
-    osg::Matrixd getMatrix() const;
-    osg::Matrixd getInverseMatrix() const;
+    osg::Matrixd getMatrix() const {return _matrix;}
+    osg::Matrixd getInverseMatrix() const {return osg::Matrixd::inverse(_matrix);}
+
+    void setAnalogDevice( Analog* aDevice ) {_analogDevice = aDevice;}
+    void setTranslateChannelX( int chan ) {_chTX = chan;}
+    void setTranslateChannelY( int chan ) {_chTY = chan;}
+    void setTranslateChannelZ( int chan ) {_chTZ = chan;}
+    void setRotateChannelX( int chan )    {_chRX = chan;}
+    void setRotateChannelY( int chan )    {_chRY = chan;}
+    void setRotateChannelZ( int chan )    {_chRZ = chan;}
+
+    /** Set/get rotation scaling factor */
+    void setRotationScale( osg::Vec3& rs ) { _rotScale = rs; }
+    osg::Vec3 getRotationScale() const { return _rotScale; }
+
+    void setButtonDevice( Button* bDevice ) {_buttonDevice = bDevice;}
+    void setResetButton( int button ) {_resetButton = button;}
 
 protected:      // methods
+    ~AnalogTracker() {}
+
     AnalogTracker(const AnalogTracker&);
-    virtual ~AnalogTracker();
     const AnalogTracker& operator=(const AnalogTracker&);
 
 protected:      // data
+    osg::ref_ptr<Analog> _analogDevice;
+    int _chTX, _chTY, _chTZ;
+    int _chRX, _chRY, _chRZ;
+    osg::Vec3 _rotScale;
+
+    osg::ref_ptr<Button> _buttonDevice;
+    int _resetButton;
+    bool _previousResetState;
+
+    osg::Matrixd _matrix;
 };
 
 }
@@ -44,7 +73,3 @@ protected:      // data
 #endif
 
 // vim: set sw=4 ts=8 et ic ai:
-
-
-
-
--- osgvrpn/trunk/OpenSceneGraph/src/osgVRPN/AnalogTracker.cpp
+++ osgvrpn/trunk/OpenSceneGraph/src/osgVRPN/AnalogTracker.cpp
@@ -1,61 +1,65 @@
 /* file:        src/osgVRPN/AnalogTracker.cpp
- * author:      Mike Weiblen mew@mew.cx
- * copyright:   (C) 2006 Michael Weiblen
+ * author:      Mike Weiblen
+ * copyright:   (C) 2006 Michael Weiblen http://mew.cx/
  * license:     OpenSceneGraph Public License (OSGPL)
- * $Id: AnalogTracker.cpp,v 1.2 2006/07/01 20:48:52 mew Exp $
+ * $Id 2006-07-08 $
 */
 
-#include <osg/Notify>
 #include <osgVRPN/AnalogTracker.h>
+#include <osg/Quat>
 
 using namespace osgVRPN;
 
 ///////////////////////////////////////////////////////////////////////////
 
-AnalogTracker::AnalogTracker()
+AnalogTracker::AnalogTracker() :
+        _analogDevice(0),
+        _chTX(-1), _chTY(-1), _chTZ(-1),
+        _chRX(-1), _chRY(-1), _chRZ(-1),
+        _rotScale(1,1,1),
+        _buttonDevice(0),
+        _resetButton(-1), _previousResetState(false)
 {
-}
-
-AnalogTracker::~AnalogTracker()
-{
-}
-
-///////////////////////////////////////////////////////////////////////////
-
-bool AnalogTracker::update()
-{
-    _updateReceivedEvent = false;
-    // update the attached osgVRPN::Analog devices
-#if 0 //[
-    _position[0] = _scale[0] * info.pos[0];
-    _position[1] = _scale[1] * info.pos[1];
-    _position[2] = _scale[2] * info.pos[2];
-    _rotation.set( info.quat[0], info.quat[1], info.quat[2], info.quat[3] );
-    if( _enabled ) ...
-#endif //]
-    return _updateReceivedEvent;
+    reset();
 }
 
 ///////////////////////////////////////////////////////////////////////////
 
-osg::Matrixd AnalogTracker::getMatrix() const
+void AnalogTracker::update()
 {
-#if 0 //[
-    return osg::Matrixd::rotate( _rotation ) *
-            osg::Matrixd::translate( _position );
-#endif //]
-    osg::Matrixd _m;
-    return _m;
-}
+    if( _analogDevice.valid() ) _analogDevice->clearEventCounter();
+    if( _buttonDevice.valid() ) _buttonDevice->clearEventCounter();
 
-osg::Matrixd AnalogTracker::getInverseMatrix() const
-{
-#if 0 //[
-    return osg::Matrixd::translate( -_position ) *
-            osg::Matrixd::rotate( _rotation.inverse() );
-#endif //]
-    osg::Matrixd _m;
-    return _m;
+    if( _analogDevice.valid() ) _analogDevice->update();
+    if( _buttonDevice.valid() ) _buttonDevice->update();
+
+    if( _analogDevice.valid() && _analogDevice->getEventCounter() > 0 )
+    {
+        Analog& ana = *(_analogDevice.get());
+
+        osg::Vec3 pos;
+        if( _chTX >= 0 ) pos[0] = _transScale[0] * ana.getValue(_chTX);
+        if( _chTY >= 0 ) pos[1] = _transScale[1] * ana.getValue(_chTY);
+        if( _chTZ >= 0 ) pos[2] = _transScale[2] * ana.getValue(_chTZ);
+
+        osg::Quat qx, qy, qz;
+        if( _chRX >= 0 ) qx.makeRotate( _rotScale[0] * ana.getValue(_chRX), osg::Vec3(1,0,0) );
+        if( _chRY >= 0 ) qy.makeRotate( _rotScale[1] * ana.getValue(_chRY), osg::Vec3(0,1,0) );
+        if( _chRZ >= 0 ) qz.makeRotate( _rotScale[2] * ana.getValue(_chRZ), osg::Vec3(0,0,1) );
+
+        _matrix = osg::Matrixd::translate(pos) * osg::Matrixd::rotate(qx*qy*qz) * _matrix;
+
+        _eventCounter += ana.getEventCounter();
+    }
+
+    if( _buttonDevice.valid() && _buttonDevice->getEventCounter() > 0 )
+    {
+        bool currentResetState = _buttonDevice->getButtonState( _resetButton );
+        if( currentResetState && !_previousResetState ) reset();
+        _previousResetState = currentResetState;
+
+        _eventCounter += _buttonDevice->getEventCounter();
+    }
 }
 
 // vim: set sw=4 ts=8 et ic ai:
