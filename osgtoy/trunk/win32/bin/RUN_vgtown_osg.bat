87d2c7214eb1db3aaf0cfa1443a37207fb400aa2
http://mew.cx/ <mew-cx@users.noreply.github.com>
2006-07-19 22:16:14 +0000

    finish the vgtown scripts

--- osgtoy/trunk/win32/bin/RUN_vgtown_osg.bat
+++ osgtoy/trunk/win32/bin/RUN_vgtown_osg.bat
@@ -4,5 +4,4 @@ REM http://www.multigen-paradigm.com/support/sc_files/VegaPlayer_NT.zip
 
 set PATH=%OSG_ROOT%\bin;%PATH%
 set OSG_FILE_PATH=E:\mew\osg\tmp\VegaPlayer_NT\demo;%OSG_FILE_PATH%
-REM I prefer the origin at the center of town
-osgconv%DEBUG% town.flt.-2590,-2500,0.trans vgtown.osg
+osgviewer%DEBUG% vgtown.osg
