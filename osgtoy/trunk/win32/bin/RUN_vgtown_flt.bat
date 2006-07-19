dd6a34907da8e1a54f5a34356b7a8279f09a8585
mew <mew@5d68af97-c12e-0410-8cc7-f28dd97b931a>
2006-07-19 22:16:14 +0000

    finish the vgtown scripts

--- osgtoy/trunk/win32/bin/RUN_vgtown_flt.bat
+++ osgtoy/trunk/win32/bin/RUN_vgtown_flt.bat
@@ -3,6 +3,5 @@ REM the "Vega Town" database is included with the VegaPlayer demo
 REM http://www.multigen-paradigm.com/support/sc_files/VegaPlayer_NT.zip
 
 set PATH=%OSG_ROOT%\bin;%PATH%
-set X=E:\TMP\DOWNLOAD\VegaPlayer_NT
-set OSG_FILE_PATH=%X%\demo;%X%\data;%OSG_FILE_PATH%
+set OSG_FILE_PATH=E:\mew\osg\tmp\VegaPlayer_NT\demo;%OSG_FILE_PATH%
 osgviewer%DEBUG% town.flt
