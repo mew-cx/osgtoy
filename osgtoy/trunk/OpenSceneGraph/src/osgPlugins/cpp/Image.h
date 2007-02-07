4ca2e49d9e363480b0225eb1119691c38950414e
http://mew.cx/ <mew-cx@users.noreply.github.com>
2007-02-07 04:00:20 +0000

    add initial version of .cpp plugin

---
+++ osgtoy/trunk/OpenSceneGraph/src/osgPlugins/cpp/Image.h
@@ -0,0 +1,23 @@
+/* file:      src/osgPlugins/cpp/Image.cpp
+ * author:    Mike Weiblen http://mew.cx/
+ * copyright: (C) 2007 Michael Weiblen
+ * license:   OpenSceneGraph Public License (OSGPL)
+ * $Id 2007-02-03 $
+*/
+
+#ifndef OSGPLUGIN_CPP_IMAGE_H
+#define OSGPLUGIN_CPP_IMAGE_H
+
+#include <iostream>
+#include <osg/Image>
+
+namespace cpp
+{
+
+std::ostream& operator << (std::ostream& fout, const osg::Image& img );
+
+}
+
+#endif
+
+// vim: set sw=4 ts=8 et ic ai:
