7dd666d84580345e6841afeda7beaee000a795e0
http://mew.cx/ <mew-cx@users.noreply.github.com>
2007-02-18 22:21:59 +0000

    initial checkin of osgfs

---
+++ osgtoy/trunk/OpenSceneGraph/src/osgPlugins/osgfs/Input.cpp
@@ -0,0 +1,113 @@
+/* -*-c++-*- OpenSceneGraph - Copyright (C) 1998-2006 Robert Osfield 
+ *
+ * This library is open source and may be redistributed and/or modified under  
+ * the terms of the OpenSceneGraph Public License (OSGPL) version 0.0 or 
+ * (at your option) any later version.  The full license is in LICENSE file
+ * included with this distribution, and on the openscenegraph.org website.
+ * 
+ * This library is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the 
+ * OpenSceneGraph Public License for more details.
+*/
+
+/* file:      src/osgPlugins/osgfs/Input.cpp
+ * author:    Mike Weiblen http://mew.cx/
+ * copyright: (C) 2007 Michael Weiblen
+ * license:   OpenSceneGraph Public License (OSGPL)
+ * $Id: Input.cpp,v 1.1 2007/02/18 22:21:59 mew Exp $
+*/
+
+#include <osg/Object>
+
+#include <osgDB/Registry>
+#include <osgDB/ReadFile>
+
+#include "Input.h"
+
+using namespace plugin_osgfs;
+
+Input::Input()
+{
+}
+
+
+Input::~Input()
+{
+}
+
+#if 0
+
+osg::Object* Input::getObjectForUniqueID(const std::string& uniqueID)
+{
+    UniqueIDToObjectMapping::iterator fitr = _uniqueIDToObjectMap.find(uniqueID);
+    if (fitr != _uniqueIDToObjectMap.end()) return (*fitr).second.get();
+    else return NULL;
+}
+
+
+void Input::regisiterUniqueIDForObject(const std::string& uniqueID,osg::Object* obj)
+{
+    _uniqueIDToObjectMap[uniqueID] = obj;
+}
+
+
+osg::Object* Input::readObjectOfType(const osg::Object& compObj)
+{
+    return Registry::instance()->readObjectOfType(compObj,*this);
+}
+
+osg::Object* Input::readObjectOfType(const basic_type_wrapper &btw)
+{
+    return Registry::instance()->readObjectOfType(btw,*this);
+}
+
+osg::Object* Input::readObject()
+{
+    return Registry::instance()->readObject(*this);
+}
+
+
+osg::Image*  Input::readImage()
+{
+    return Registry::instance()->readImage(*this);
+}
+
+osg::Drawable* Input::readDrawable()
+{
+    return Registry::instance()->readDrawable(*this);
+}
+
+osg::StateAttribute* Input::readStateAttribute()
+{
+    return Registry::instance()->readStateAttribute(*this);
+}
+osg::Uniform* Input::readUniform()
+{
+    return Registry::instance()->readUniform(*this);
+}
+
+osg::Node* Input::readNode()
+{
+    return Registry::instance()->readNode(*this);
+}
+
+osg::Object* Input::readObject(const std::string& fileName)
+{
+    return readObjectFile(fileName,_options.get());
+}
+
+osg::Image*  Input::readImage(const std::string& fileName)
+{
+
+    return readImageFile(fileName,_options.get());
+}
+
+osg::Node* Input::readNode(const std::string& fileName)
+{
+    return readNodeFile(fileName,_options.get());
+}
+
+#endif
+
+// vim: set sw=4 ts=8 et ic ai:
