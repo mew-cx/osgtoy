71268d825eb5dcdb730ebc724a0e6f1f7580d098
http://mew.cx/ <mew-cx@users.noreply.github.com>
2007-06-07 12:35:19 +0000

    enhance script

--- osgbuilds/trunk/RUN_FULL_BUILD.bat
+++ osgbuilds/trunk/RUN_FULL_BUILD.bat
@@ -1,14 +1,19 @@
+@echo off
 title %0
 call SETENV.bat
 
-@echo START
-@date /t
-@time /t
+echo START
+date /t
+time /t
+echo .
 
+echo Running full VisualStudio batch build:
+echo solution = %SOLUTION%
+echo buildconfig = %BUILDCFG%
 %VSIDE% /build %BUILDCFG% /project INSTALL %SOLUTION%
 
-@echo DONE
-@date /t
-@time /t
-
-@pause
+echo .
+echo DONE
+date /t
+time /t
+pause
