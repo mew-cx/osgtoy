116aab98dc683ef85e10002f138837c6c97b1f98
mew <mew@5d68af97-c12e-0410-8cc7-f28dd97b931a>
2007-06-19 07:54:33 +0000

    tagged for osg 2.0.0

---
+++ osgtoy-data/tags/osgtoy-data_for_osg_2_0_0/ss3d_0011.lua
@@ -0,0 +1,9 @@
+-- see http://astronomy.swin.edu.au/~pbourke/surfaces/supershape3d/
+
+return osgToy.SuperShape3D {
+
+name="c_0011",
+resolution = 128,
+ss1_m=8, ss1_n1=60, ss1_n2=100, ss1_n3=30, ss1_a=1, ss1_b=1,
+ss2_m=2, ss2_n1=10, ss2_n2=10, ss2_n3=10, ss2_a=1, ss2_b=1,
+}
---
+++ osgtoy-data/tags/osgtoy-data_for_osg_2_0_0/ss3d_0019.lua
@@ -0,0 +1,9 @@
+-- see http://astronomy.swin.edu.au/~pbourke/surfaces/supershape3d/
+
+return osgToy.SuperShape3D {
+
+name="c_0019",
+resolution = 128,
+ss1_m=4, ss1_n1=0.375393, ss1_n2=50.7442, ss1_n3=-0.657, ss1_a=1, ss1_b=1,
+ss2_m=9, ss2_n1=-74.1592, ss2_n2=-0.68202, ss2_n3=-41.1, ss2_a=1, ss2_b=1,
+}
