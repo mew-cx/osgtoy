dc72e4bd86672b9753eaa2c562e75cbabd813114
http://mew.cx/ <mew-cx@users.noreply.github.com>
2007-06-19 07:54:33 +0000

    tagged for osg 2.0.0

---
+++ osgtoy-data/tags/osgtoy-data_for_osg_2_0_0/ss3d_cube.lua
@@ -0,0 +1,19 @@
+-- construct a SuperShape using the osgToy .lua plugin
+-- see http://astronomy.swin.edu.au/~pbourke/surfaces/supershape3d/
+
+return osgToy.SuperShape3D {
+    name =	"cube",
+    resolution = 128,
+    ss1_m =	4,
+    ss1_a =	1,
+    ss1_b =	1,
+    ss1_n1 =	100,
+    ss1_n2 =	100,
+    ss1_n3 =	100,
+    ss2_m =	4,
+    ss2_a =	1,
+    ss2_b =	1,
+    ss2_n1 =	100,
+    ss2_n2 =	100,
+    ss2_n3 =	100,
+}
