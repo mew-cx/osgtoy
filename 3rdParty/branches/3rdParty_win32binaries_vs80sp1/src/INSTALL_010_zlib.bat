3a601fc5d7140d1e4586720d07c278d114bf6a5d
http://mew.cx/ <mew-cx@users.noreply.github.com>
2007-06-23 16:25:54 +0000

    3rdParty wip

--- 3rdParty/branches/3rdParty_win32binaries_vs80sp1/src/INSTALL_010_zlib.bat
+++ 3rdParty/branches/3rdParty_win32binaries_vs80sp1/src/INSTALL_010_zlib.bat
@@ -1,22 +1,27 @@
 @echo off
 call PACKAGE_VERSIONS.bat
 
-set SRCDIR=%CD%\%DIR_ZLIB%
-
-if NOT EXIST %SRCDIR%\zlib.h (
-echo ERROR %PKG_ZLIB% must be unpacked to %SRCDIR%
+if NOT EXIST %DIR_ZLIB%\zlib.h (
+echo ERROR %PKG_ZLIB% must be unpacked to %DIR_ZLIB%
 pause
 exit
 )
 
-if NOT EXIST foo (
+if NOT EXIST %DIR_PNG%\projects\visualc80\Win32_DLL_Release\ZLib\zlib1.dll (
 echo ERROR zlib will be built as part of libpng.
 echo build libpng first, then rerun this script.
 pause
 exit
 )
 
-echo TODO do copy here
+copy %DIR_PNG%\projects\visualc80\Win32_DLL_Release\ZLib\zlib1.dll %BIN_DIR%
+copy %DIR_PNG%\projects\visualc80\Win32_DLL_Debug\ZLib\zlib1d.dll %BIN_DIR%
+
+copy %DIR_PNG%\projects\visualc80\Win32_DLL_Release\ZLib\zlib1.lib %LIB_DIR%
+copy %DIR_PNG%\projects\visualc80\Win32_DLL_Debug\ZLib\zlib1d.lib %LIB_DIR%
+
+copy %DIR_ZLIB%\zlib.h %INC_DIR%
+copy %DIR_ZLIB%\zconf.h %INC_DIR%
 
 echo OK zlib installed
 pause
--- 3rdParty/branches/3rdParty_win32binaries_vs80sp1/src/PACKAGE_VERSIONS.bat
+++ 3rdParty/branches/3rdParty_win32binaries_vs80sp1/src/PACKAGE_VERSIONS.bat
@@ -1,56 +1,67 @@
 REM list of exact 3rdParty source packages
 REM mew 2007-06-23
 
-set DIR_ZLIB=zlib
+REM where build products are installed...
+set BIN_DIR=%CD%\..\bin
+set INC_DIR=%CD%\..\include
+set LIB_DIR=%CD%\..\lib
+
+
+REM list of source packages...
+
+set DIR_REDIST=C:\Program Files\Microsoft Visual Studio 8\VC\redist\x86
+set DIR_REDIST_CRT=%DIR_REDIST%\Microsoft.VC80.CRT
+set DIR_REDIST_MFC=%DIR_REDIST%\Microsoft.VC80.MFC
+
+set DIR_ZLIB=%CD%\zlib
 set PKG_ZLIB=%DIR_ZLIB%123.zip
 set URL_ZLIB=http://www.zlib.net/%PKG_ZLIB%
 
-set DIR_PNG=lpng1218
+set DIR_PNG=%CD%\lpng1218
 set PKG_PNG=%DIR_PNG%.zip
 set URL_PNG=http://downloads.sourceforge.net/libpng/%PKG_PNG%
 
 set DIR_JPEG=
-set PKG_JPEG=jpegsr6.zip
+set PKG_JPEG=%CD%\jpegsr6.zip
 set URL_JPEG=ftp://ftp.simtel.net/pub/simtelnet/msdos/graphics/%PKG_JPEG%
 
 set DIR_LUA=
-set PKG_LUA=lua-5.1.2.tar.gz
+set PKG_LUA=%CD%\lua-5.1.2.tar.gz
 set URL_LUA=http://www.lua.org/ftp/%PKG_LUA%
 
-set DIR_UNGIF=
+set DIR_UNGIF=%CD%\
 set PKG_UNGIF=libungif-4.1.4.tar.gz
 set URL_UNGIF=http://downloads.sourceforge.net/libungif/%PKG_UNGIF%
 
 REM ------------------------------
 
-set DIR_FREETYPE=
+set DIR_FREETYPE=%CD%\
 set PKG_FREETYPE=ft234.zip
 set URL_FREETYPE=http://downloads.sourceforge.net/freetype/%PKG_FREETYPE%
 
-set DIR_GLUT=
+set DIR_GLUT=%CD%\
 set PKG_GLUT=glut-3.7.6-src.zip
 set URL_GLUT=http://www.xmission.com/~nate/glut/%PKG_GLUT%
 
 REM ------------------------------
 
-set DIR_TIFF=
+set DIR_TIFF=%CD%\
 set PKG_TIFF=tiff-3.8.2.zip
 set URL_TIFF=ftp://ftp.remotesensing.org/pub/libtiff/%PKG_TIFF%
 
-set DIR_VRPN=
+set DIR_VRPN=%CD%\
 set PKG_VRPN=vrpn_07_08.zip
 set URL_VRPN=ftp://ftp.cs.unc.edu/pub/packages/GRIP/vrpn/%PKG_VRPN%
 
-set DIR_GLSLFE=
+set DIR_GLSLFE=%CD%\
 set PKG_GLSLFE=OpenGLCompilerSept202005.zip
 set URL_GLSLFE=http://developer.3dlabs.com/downloads/glslcompiler/%PKG_GLSLFE%
 
-set DIR_PROJ=
+set DIR_PROJ=%CD%\
 set PKG_PROJ=proj-4.5.0.zip
 set URL_PROJ=ftp://ftp.remotesensing.org/proj/%PKG_PROJ%
 
-set DIR_GDAL=
+set DIR_GDAL=%CD%\
 set PKG_GDAL=gdal141.zip
 set URL_GDAL=http://download.osgeo.org/gdal/%PKG_GDAL%
-
 
