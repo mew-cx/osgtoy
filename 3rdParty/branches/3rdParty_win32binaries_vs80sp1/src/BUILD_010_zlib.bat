51467fca8e96b05de2f5efe32862388c298b4c43
mew <mew@5d68af97-c12e-0410-8cc7-f28dd97b931a>
2007-06-23 13:36:52 +0000

    3rdParty wip

--- 3rdParty/branches/3rdParty_win32binaries_vs80sp1/src/BUILD_010_zlib.bat
+++ 3rdParty/branches/3rdParty_win32binaries_vs80sp1/src/BUILD_010_zlib.bat
@@ -2,11 +2,20 @@
 
 call PACKAGE_VERSIONS.bat
 
-set SRCDIR=%CD%\zlib
+set SRCDIR=%CD%\%DIR_ZLIB%
 
 if NOT EXIST %SRCDIR% (
-echo ERROR: %PKG_ZLIB% must be unpacked at %SRCDIR%
+echo ERROR %PKG_ZLIB% must be unpacked to %SRCDIR%
 pause
 exit
 )
 
+if NOT EXIST foo (
+echo ERROR zlib is built as part of libpng.
+echo rerun this script after building libpng.
+pause
+exit
+)
+
+echo OK zlib successfully installed
+pause
