9a31ab44fa64c3e630b0f17ead04e7e23579e74c
http://mew.cx/ <mew-cx@users.noreply.github.com>
2007-06-27 11:38:56 +0000

    add ungif

--- 3rdParty/branches/3rdParty_win32binaries_vs80sp1/src/libungif-4.1.4/INSTALL_libungif.bat
+++ 3rdParty/branches/3rdParty_win32binaries_vs80sp1/src/libungif-4.1.4/INSTALL_libungif.bat
@@ -1,8 +1,1 @@
-REM first, build using windows\libungif.sln
-REM then...
-
-copy windows\Debug\libungifD.lib  ..\..\lib
-copy windows\Release\libungif.lib  ..\..\lib
-copy lib\gif_lib.h  ..\..\include
-
-pause
+delete me
