3e0df70a65c3772680066385c25a370ee52fb2f7
http://mew.cx/ <mew-cx@users.noreply.github.com>
2007-10-08 16:50:14 +0000

    update to freetype 2.3.5

--- 3rdParty/branches/3rdParty_win32binaries_vs80sp1/src/x2070_freetype.bat
+++ 3rdParty/branches/3rdParty_win32binaries_vs80sp1/src/x2070_freetype.bat
@@ -7,7 +7,7 @@ pause
 exit
 )
 
-if NOT EXIST %DIR_FREETYPE%\objs\freetype234.lib (
+if NOT EXIST %DIR_FREETYPE%\objs\freetype235.lib (
 echo ERROR first must build freetype using %DIR_FREETYPE%\builds\win32\visualc\freetype.sln
 echo then rerun this script.
 pause
@@ -17,8 +17,8 @@ exit
 echo copying products to %INSTALL_ROOT%
 echo on
 
-copy %DIR_FREETYPE%\objs\freetype234.lib %INSTALL_LIB%
-copy %DIR_FREETYPE%\objs\freetype234_D.lib %INSTALL_LIB%
+copy %DIR_FREETYPE%\objs\freetype235.lib %INSTALL_LIB%
+copy %DIR_FREETYPE%\objs\freetype235_D.lib %INSTALL_LIB%
 
 xcopy /e %DIR_FREETYPE%\include\* %INSTALL_INC%
 
