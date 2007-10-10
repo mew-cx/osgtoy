c18133dc6c0c258aa01f943c315dfac113d02b66
http://mew.cx/ <mew-cx@users.noreply.github.com>
2007-10-10 05:17:17 +0000

    tagged for release

---
+++ osgtoy-data/tags/osg2.2.0_vs80_setup_2007-10-08/ss3d_0012.lua
@@ -0,0 +1,9 @@
+-- see http://astronomy.swin.edu.au/~pbourke/surfaces/supershape3d/
+
+return osgToy.SuperShape3D {
+
+name="c_0012",
+resolution = 128,
+ss1_m=3, ss1_n1=0.5, ss1_n2=1.7, ss1_n3=1.7, ss1_a=1, ss1_b=1,
+ss2_m=2, ss2_n1=10, ss2_n2=10, ss2_n3=10, ss2_a=1, ss2_b=1,
+}
---
+++ osgtoy-data/tags/osg2.2.0_vs80_setup_2007-10-08/ss3d_0016.lua
@@ -0,0 +1,9 @@
+-- see http://astronomy.swin.edu.au/~pbourke/surfaces/supershape3d/
+
+return osgToy.SuperShape3D {
+
+name="c_0016",
+resolution = 128,
+ss1_m=5.7, ss1_n1=0.5, ss1_n2=1, ss1_n3=2.5, ss1_a=1, ss1_b=1,
+ss2_m=10, ss2_n1=3, ss2_n2=0.2, ss2_n3=1, ss2_a=1, ss2_b=1,
+}
