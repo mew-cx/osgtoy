5390636549b9909ab40f9b843f9324c2535189af
mew <mew@5d68af97-c12e-0410-8cc7-f28dd97b931a>
2007-10-16 04:30:34 +0000

    patch from Hartmut Seichter: make osgVRPN::Tracker consider tracker devices with more than one sensor channel.

--- osgvrpn/trunk/src/osgVRPN/Tracker.cpp
+++ osgvrpn/trunk/src/osgVRPN/Tracker.cpp
@@ -1,8 +1,8 @@
 /* file:        src/osgVRPN/Tracker.cpp
  * author:      Mike Weiblen
- * copyright:   (C) 2003-2006 Michael Weiblen http://mew.cx/
+ * copyright:   (C) 2003-2007 Michael Weiblen http://mew.cx/
  * license:     OpenSceneGraph Public License (OSGPL)
- * $Id: Tracker.cpp,v 1.8 2006/07/15 23:54:58 mew Exp $
+ * $Id$
 */
 
 #include <osgVRPN/Tracker.h>
@@ -14,8 +14,9 @@ using namespace osgVRPN;
 
 ///////////////////////////////////////////////////////////////////////////
 
-Tracker::Tracker( const char* deviceName ) :
-        _vrpnTracker( new vrpn_Tracker_Remote(deviceName) )
+Tracker::Tracker( const char* deviceName, int sensorID ) :
+        _vrpnTracker( new vrpn_Tracker_Remote(deviceName) ),
+        _sensorID( sensorID )
 {
     _vrpnTracker->register_change_handler( this, s_ChangeHandler );
 }
@@ -58,10 +59,13 @@ void Tracker::update()
 
 void Tracker::changeHandler( const vrpn_TRACKERCB& info )
 {
-    _position[0] = _transScale[0] * info.pos[0];
-    _position[1] = _transScale[1] * info.pos[1];
-    _position[2] = _transScale[2] * info.pos[2];
-    _rotation.set( info.quat[0], info.quat[1], info.quat[2], info.quat[3] );
+    if( (_sensorID == -1) || (_sensorID == info.sensor) )
+    {
+        _position[0] = _transScale[0] * info.pos[0];
+        _position[1] = _transScale[1] * info.pos[1];
+        _position[2] = _transScale[2] * info.pos[2];
+        _rotation.set( info.quat[0], info.quat[1], info.quat[2], info.quat[3] );
+    }
 
     ++_eventCounter;
 }
