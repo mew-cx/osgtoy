34cfbe1e7c4c8a1bf60f98e8a1b43c10e4e4cc3a
http://mew.cx/ <mew-cx@users.noreply.github.com>
2008-04-26 15:05:37 +0000

    create branch for OSG 2.4.0 release

---
+++ osgbuilds/branches/build_for_osg_2_4_0/win32/bin/RUN_osgdem_bluemarble.bat
@@ -0,0 +1,24 @@
+REM demonstration of osgdem, derived from:
+REM http://www.andesengineering.com/BlueMarbleViewer/
+REM mew 2005-11-22
+
+title %0
+
+set PATH=%OSG_PATH%;%PATH%
+
+REM where to find the source data
+set SRC_DIR=C:\mew\data\bluemarble
+set SRC_EAST=%SRC_DIR%\land_shallow_topo_east.tif
+set SRC_WEST=%SRC_DIR%\land_shallow_topo_west.tif
+
+set NUM_LVL=12
+
+REM where to put the build products
+set TGT_DIR=C:\mew\data\osg\bluemarble%NUM_LVL%
+set TGT_IVE=%TGT_DIR%\earth.ive
+
+if not exist %TGT_DIR% mkdir %TGT_DIR%
+
+osgdem --bluemarble-west -t %SRC_WEST% --bluemarble-east -t %SRC_EAST% --geocentric -l %NUM_LVL% -o %TGT_IVE%
+
+pause
---
+++ osgbuilds/branches/build_for_osg_2_4_0/win32/bin/RUN_ss3d.bat
@@ -0,0 +1,9 @@
+title %0
+set PATH=%OSG_PATH%;%PATH%
+
+osgviewer ss3d_5star.lua
+osgviewer ss3d_cube.lua
+osgviewer ss3d_default.lua
+osgviewer ss3d_trillium.lua
+osgviewer ss3d_umbrella.lua
+osgviewer ss3d_urchin.lua
