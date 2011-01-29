5c3243e34506ce253478c28940facf461aeb33ca
http://mew.cx/ <mew-cx@users.noreply.github.com>
2011-01-29 17:56:52 +0000

    merge SvnKeywords into Version

--- osgtoy/trunk/src/osgToy/Version.cpp
+++ osgtoy/trunk/src/osgToy/Version.cpp
@@ -1,14 +1,46 @@
+// $Id$
+// Subversion's keyword substitution must be enabled to automatically update these values:
+//      svn propset svn:keywords "Id Date Author URL Revision" Version.cpp
+
 #include <osgToy/Version.h>
+
 
 const char* osgToyGetVersion()
 {
-    return "0.3.0";
+    return "0.4.0";
 }
-
 
 const char* osgToyGetLibraryName()
 {
-    return "Open Scene Graph Toy Library";
+    return "Open Scene Graph Toy Library from http://mew.cx/";
 }
+
+
+
+const char* osgToySvnKeywordId()
+{
+    return "$Id$";
+}
+
+const char* osgToySvnKeywordDate()
+{
+    return "$Date$";
+}
+
+const char* osgToySvnKeywordAuthor()
+{
+    return "$Author$";
+}
+
+const char* osgToySvnKeywordUrl()
+{
+    return "$URL$";
+}
+
+const char* osgToySvnKeywordRevision()
+{
+    return "$Revision$";
+}
+
 
 // vim: set sw=4 ts=8 et ic ai:
